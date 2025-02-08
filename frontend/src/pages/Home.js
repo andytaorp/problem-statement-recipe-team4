1eaaafc915c57ddbff9b35322597759189454a22
LiTianXinEden <23018351@myrp.edu.sg>
2025-02-09 01:46:37 +0800

    recipes display on frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,4 +1,4 @@
-import { useEffect }from 'react'
+import { useEffect } from 'react'
 import { useRecipeContext } from "../hooks/useRecipeContext"
 import { useAuthContext } from "../hooks/useAuthContext"
 
@@ -7,30 +7,30 @@ import RecipeDetails from '../components/RecipeDetails'
 import RecipeForm from '../components/RecipeForm'
 
 const Home = () => {
-  const {recipe, dispatch} = useRecipeContext()
-  const {user} = useAuthContext()
+  const { recipes, dispatch } = useRecipeContext()
+  const { user } = useAuthContext()
 
   useEffect(() => {
     const fetchRecipe = async () => {
       const response = await fetch(`${process.env.REACT_APP_API_URL}/api/recipes`, {
-        headers: {'Authorization': `Bearer ${user.token}`},
+        headers: { 'Authorization': `Bearer ${user.token}` },
       })
       const json = await response.json()
 
       if (response.ok) {
-        dispatch({type: 'SET_RECIPES', payload: json})
+        dispatch({ type: 'SET_RECIPES', payload: json })
       }
     }
 
     if (user) {
-        fetchRecipe()
+      fetchRecipe()
     }
   }, [dispatch, user])
 
   return (
     <div className="home">
       <div className="recipes">
-        {recipe && recipe.map((r) => (
+        {recipes && recipes.map((r) => (
           <RecipeDetails key={r._id} recipe={r} />
         ))}
       </div>
